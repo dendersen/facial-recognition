bf1eaec3c2b576d20deb7a256b261e7dc3a323a9
Chrisser1 <chrisgthomsen0310@gmail.com>
2023-03-23 21:41:58 +0100

    rewriteDataToMatchNetwork

--- SRC/AI/trainAI.py
+++ SRC/AI/trainAI.py
@@ -2,6 +2,51 @@ import tensorflow as tf
 import keras
 from keras.models import Model
 from keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import os
+import glob
+import cv2
+
+def rewriteDataToMatchNetwork(person):
+  posPath = os.path.join('images\DataSiameseNetwork','positive')
+  for f in files:
+    os.remove(f)
+  negPath = os.path.join('images\DataSiameseNetwork','negative')
+  for f in files:
+    os.remove(f)
+  ancPath = os.path.join('images\DataSiameseNetwork','anchor')
+  for f in files:
+    os.remove(f)
+  
+  
+  # Get all negative data
+  for picture in os.listdir('images\modified\Other'):
+    if ".jpg" in picture:
+      path = os.path.join('images\modified\Other', picture)
+      img = cv2.imread(path)
+      newPath = os.path.join(negPath, picture)
+      cv2.imwrite(newPath, img)
+  
+  # Get all anchor data
+  datapath = os.path.join('images/original/', person)
+  for picture in os.listdir(datapath):
+    if ".jpg" in picture:
+      path = os.path.join(datapath, picture)
+      img = cv2.imread(path)
+      newPath = os.path.join(ancPath, picture)
+      cv2.imwrite(newPath, img)
+  
+  # Get all positive data
+  datapath2 = os.path.join('images/modified/', person)
+  for picture in os.listdir(datapath2):
+    if ".jpg" in picture:
+      path = os.path.join(datapath2, picture)
+      img = cv2.imread(path)
+      newPath = os.path.join(posPath, picture)
+      cv2.imwrite(newPath, img)
+
+def buildData():
+  
+  pass
 
 def makeImbedding():
     
@@ -26,6 +71,18 @@ def makeImbedding():
     
     
     return Model(inputs=[inp], outputs=[d1], name='embedding')
+
+@tf.function # Compiles into a tensorflow graph
+def trainStep(batch):
+  
+  with tf.GradientTape() as tape: # Allows for capture of gradients from network
+    
+    # Get anchor and the positive/negative image
+    X = batch[:2]
+    # Get label
+    y = batch[2]
+  
+  pass
 
 class L1Dist(Layer):
   
@@ -56,13 +113,19 @@ class AI:
     
     return Model(inputs=[inputImage, validationImage], outputs=classifier, name='SiameseNetwork')
 
-  def trainAI():
+  def trainAI(siameseModel):
     # Define loss funktion
     binaryCrossLoss = tf.losses.BinaryCrossentropy(from_logits=True)
-    
     # Define optimizer
-    opt = tf.keras.optimizers.Adam
+    opt = tf.keras.optimizers.Adam(1e-4) # 0.0001 learningrate
     
+    # Establish checkpoints for training
+    checkpointDir = './trainingCheckpoints'
+    checkpointPrefix = os.path.join(checkpointDir, 'ckpt')
+    checkpoint = tf.train.Checkpoint(opt=opt, siameseModel=siameseModel)
+    
+    # Build the training step
+  
   # # Load the data
   # trainData = keras.utils.image_dataset_from_directory('images\modified',
   #                                                         shuffle=True,
@@ -71,7 +134,8 @@ class AI:
   pass
 
 
+rewriteDataToMatchNetwork("Niels")
 
+# siameseNetwork = AI.makeSiameseModel()
+# siameseNetwork.summary()
 
-siameseNetwork = AI.makeSiameseModel()
-siameseNetwork.summary()

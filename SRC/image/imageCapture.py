8dd239a8eb850cf4dc0f64eada8a575e60b4ace6
Chrisser1 <chrisgthomsen0310@gmail.com>
2023-03-31 11:53:39 +0200

    This is it

--- SRC/image/imageCapture.py
+++ SRC/image/imageCapture.py
@@ -36,10 +36,10 @@ class Cam:
     if type(face) != np.ndarray:
       print("there is no face!")
     else:
-      xLeft = int(min(face[0][0], face[0][2]))-10
-      xRight = int(max(face[0][0], face[0][2]))+10
-      yBottom = int(min(face[0][1], face[0][3]))-10
-      yTop = int(max(face[0][1], face[0][3]))+10
+      xLeft = int(min(face[0][0], face[0][2]))-20
+      xRight = int(max(face[0][0], face[0][2]))+20
+      yBottom = int(min(face[0][1], face[0][3]))-20
+      yTop = int(max(face[0][1], face[0][3]))+20
       
       faceWidth = xRight-xLeft
       faceHeight = yTop-yBottom

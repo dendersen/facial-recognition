8e56321a67f862f33d021d8bebefdc9c39268cbb
dendersen <dendersen@endersen.dk>
2023-04-20 18:37:48 +0200

    first attempt added

--- SRC/image/imageEditor.py
+++ SRC/image/imageEditor.py
@@ -3,6 +3,10 @@ from SRC.image.imageLoader import ProcessOther, loadImgAsArr
 from typing import List, Tuple
 import tensorflow as tf
 import os
+from typing import List
+import cv2 as cv
+import numpy as np
+from SRC.image.imageCapture import Camera
 # TODO lav en funktion som:
 # tager imod et kvadratisk billede af n- størelse af typen nr array 
 # billedet har er en buffer rundt om ansigtet
@@ -92,5 +96,47 @@ def modifyOriginals(maximum:int = 300,varients:int = 10,dataset:bool = False):
   if(dataset):
     ProcessOther()
 
+def  sharpen(pic:List[List[List[int]]]):
+  strength = 0.2
+  threashold = 0.5
 
 
+
+  cv.imshow('sharp output: ', cv.cvtColor(cv.convertScaleAbs(np.array(pic)), cv.COLOR_HSV2BGR))
+  print ("press space")
+  while(not cv.waitKey(10) == 32):pass
+  print("finish")
+
+def smooth(pic:List[List[List[int]]],strong:float = 1.0,central:float = 1.0) -> List[List[List[int]]]:
+  tempList = list
+  for y in range(len(pic)-2):
+    for x in range(len(pic[y])-2):
+      temp = (
+        float(pic[y][x][2])/(255.0)*strong+
+        float(pic[y+1][x][2])/(255.0)*strong+
+        float(pic[y+2][x][2])/(255.0)*strong+
+        float(pic[y][x+1][2])/(255.0)*strong+
+        float(pic[y+1][x+1][2])/(255.0)*central+
+        float(pic[y+2][x+1][2])/(255.0)*strong+
+        float(pic[y][x+2][2])/(255.0)*strong+
+        float(pic[y+1][x+2][2])/(255.0)*strong+
+        float(pic[y+2][x+2][2])/(255.0)*strong
+      )/(central+8*strong)
+    tempList[y+1][x+1][2] = int(temp*255) > tempList[y+1][x+1][2]
+  return tempList
+
+def difference(pic1:List[List[List[int]]],pic2:List[List[List[int]]]):
+  for ny,ty in zip(range(len(pic1)),range(len(pic2))):
+    for nx,tx in zip(range(len(pic1[ny])),range(len(pic2[ty]))):
+      pic2[ty][tx][2] = abs(pic1[ty][tx][2] - pic2[ny][nx][2])
+
+def combine(pic1,Pic2, strength:float):
+  for ny,ty in zip(range(len(pic1)),range(len(Pic2))):
+    for nx,tx in zip(range(len(pic1[ny])),range(len(Pic2[ty]))):
+      temp = (int(float(pic1[ty][tx][2]) * (1-strength)) + int(strength * float(Pic2[ny][nx][2])))
+      if(temp <= 255 and pic1[ty][tx][2] >= 0):
+        pic1[ty][tx][2] = temp
+      elif(temp > 255):
+        pic1[ty][tx][2] = 255
+      else:
+        pic1[ty][tx][2] = 0

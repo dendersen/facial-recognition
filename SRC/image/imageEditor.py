d93f6ad27aefef112a9fb63efaaf63f0fcdc0a9e
dendersen <dendersen@endersen.dk>
2023-04-20 21:15:48 +0200

    made image sharpener (not perfect)

--- SRC/image/imageEditor.py
+++ SRC/image/imageEditor.py
@@ -6,7 +6,6 @@ import os
 from typing import List
 import cv2 as cv
 import numpy as np
-from SRC.image.imageCapture import Camera
 # TODO lav en funktion som:
 # tager imod et kvadratisk billede af n- størelse af typen nr array 
 # billedet har er en buffer rundt om ansigtet
@@ -26,23 +25,23 @@ import random
 from SRC.image.imageSaver import saveImage
 
 def makeVarients(image: List[List[List[int]]], variantNumber:int = 10) -> List[List[List[List[int]]]]: 
-    # Height and width is always the same. It's defined as size
-    size  = image.shape[0]
+  # Height and width is always the same. It's defined as size
+  size  = image.shape[0]
+  
+  buffer1Size = int(size * 0.8)
+  maxOffset = int(size-buffer1Size)
+  
+  faces: list[np.ndarray[np.ndarray[np.ndarray[int]]]] = []
+  
+  for i in range(variantNumber):
+    xStart =  random.randint(0,maxOffset)
+    xEnd = xStart + buffer1Size
+    yStart = random.randint(0,maxOffset)
+    yEnd = yStart + buffer1Size
+    newVariant = image[yStart:yEnd, xStart:xEnd]
     
-    buffer1Size = int(size * 0.8)
-    maxOffset = int(size-buffer1Size)
-    
-    faces: list[np.ndarray[np.ndarray[np.ndarray[int]]]] = []
-    
-    for i in range(variantNumber):
-        xStart =  random.randint(0,maxOffset)
-        xEnd = xStart + buffer1Size
-        yStart = random.randint(0,maxOffset)
-        yEnd = yStart + buffer1Size
-        newVariant = image[yStart:yEnd, xStart:xEnd]
-        
-        faces.append(newVariant)
-    return faces
+    faces.append(newVariant)
+  return faces
 
 def clearModified():
   clearPath('images\\modified\\Christoffer')
@@ -96,47 +95,61 @@ def modifyOriginals(maximum:int = 300,varients:int = 10,dataset:bool = False):
   if(dataset):
     ProcessOther()
 
-def  sharpen(pic:List[List[List[int]]]):
-  strength = 0.2
-  threashold = 0.5
+def sharpen(pic:List[List[List[int]]], strength:float = 0.2,threshold = -1, showSteps:bool = False, showEnd:bool = False):
+  process = smooth(pic,threshold)
+  if(showSteps):
+    cv.imshow('smooth output: ',cv.convertScaleAbs(np.array(process)))
+  process = difference(pic,process)
+  if(showSteps):
+    cv.imshow('detail output: ',cv.convertScaleAbs(np.array(process)))
+  pic = combine(pic,process,strength,threshold)
+  if(showEnd):
+    cv.imshow('sharp output: ',cv.convertScaleAbs(np.array(pic)))
+  return pic
 
-
-
-  cv.imshow('sharp output: ', cv.cvtColor(cv.convertScaleAbs(np.array(pic)), cv.COLOR_HSV2BGR))
-  print ("press space")
-  while(not cv.waitKey(10) == 32):pass
-  print("finish")
-
-def smooth(pic:List[List[List[int]]],strong:float = 1.0,central:float = 1.0) -> List[List[List[int]]]:
-  tempList = list
+def smooth(pic:List[List[List[int]]],threshold = -1,strong:float = 1.0,central:float = 1.0) -> List[List[List[int]]]:
+  orgPic = cv.cvtColor(pic, cv.COLOR_BGR2HSV)
+  tempList = orgPic.copy()
   for y in range(len(pic)-2):
     for x in range(len(pic[y])-2):
-      temp = (
-        float(pic[y][x][2])/(255.0)*strong+
-        float(pic[y+1][x][2])/(255.0)*strong+
-        float(pic[y+2][x][2])/(255.0)*strong+
-        float(pic[y][x+1][2])/(255.0)*strong+
-        float(pic[y+1][x+1][2])/(255.0)*central+
-        float(pic[y+2][x+1][2])/(255.0)*strong+
-        float(pic[y][x+2][2])/(255.0)*strong+
-        float(pic[y+1][x+2][2])/(255.0)*strong+
-        float(pic[y+2][x+2][2])/(255.0)*strong
-      )/(central+8*strong)
-    tempList[y+1][x+1][2] = int(temp*255) > tempList[y+1][x+1][2]
-  return tempList
+      for col in range(len(pic[y][x])):
+        tempList[y+1][x+1][col] = int(((
+          float(orgPic[y][x][col])/(255.0)*strong+
+          float(orgPic[y+1][x][col])/(255.0)*strong+
+          float(orgPic[y+2][x][col])/(255.0)*strong+
+          float(orgPic[y][x+1][col])/(255.0)*strong+
+          float(orgPic[y+1][x+1][col])/(255.0)*central+
+          float(orgPic[y+2][x+1][col])/(255.0)*strong+
+          float(orgPic[y][x+2][col])/(255.0)*strong+
+          float(orgPic[y+1][x+2][col])/(255.0)*strong+
+          float(orgPic[y+2][x+2][col])/(255.0)*strong
+        )/(central+8*strong))*255)
+        if(abs(int(tempList[y][x][col]) - int(orgPic[y][x][col])) < threshold):
+          tempList[y][x][col] = orgPic[y][x][col]
+  return cv.cvtColor(tempList, cv.COLOR_HSV2BGR)
 
-def difference(pic1:List[List[List[int]]],pic2:List[List[List[int]]]):
-  for ny,ty in zip(range(len(pic1)),range(len(pic2))):
-    for nx,tx in zip(range(len(pic1[ny])),range(len(pic2[ty]))):
-      pic2[ty][tx][2] = abs(pic1[ty][tx][2] - pic2[ny][nx][2])
+def difference(pic1:List[List[List[int]]],pic2:List[List[List[int]]]) -> List[List[List[int]]]:
+  tempPic1 = cv.cvtColor(pic1, cv.COLOR_BGR2HSV)
+  tempPic2 = cv.cvtColor(pic2, cv.COLOR_BGR2HSV)
+  for ny,ty in zip(range(len(tempPic1)),range(len(tempPic2))):
+    for nx,tx in zip(range(len(tempPic1[ny])),range(len(tempPic2[ty]))):
+      tempPic1[ty][tx][0] = int(abs(int(tempPic1[ty][tx][0]) - int(tempPic2[ny][nx][0])))
+      tempPic1[ty][tx][1] = int(abs(int(tempPic1[ty][tx][1]) - int(tempPic2[ny][nx][1])))
+      tempPic1[ty][tx][2] = int(abs(int(tempPic1[ty][tx][2]) - int(tempPic2[ny][nx][2])))
+  
+  return cv.cvtColor(tempPic1, cv.COLOR_HSV2BGR)
 
-def combine(pic1,Pic2, strength:float):
-  for ny,ty in zip(range(len(pic1)),range(len(Pic2))):
-    for nx,tx in zip(range(len(pic1[ny])),range(len(Pic2[ty]))):
-      temp = (int(float(pic1[ty][tx][2]) * (1-strength)) + int(strength * float(Pic2[ny][nx][2])))
-      if(temp <= 255 and pic1[ty][tx][2] >= 0):
-        pic1[ty][tx][2] = temp
-      elif(temp > 255):
-        pic1[ty][tx][2] = 255
-      else:
-        pic1[ty][tx][2] = 0
+def combine(pic1:List[List[List[int]]],pic2:List[List[List[int]]], strength:float, threshold = 0) -> List[List[List[int]]]:
+  tempPic1 = pic1
+  tempPic2 = pic2
+  for ny,ty in zip(range(len(tempPic1)),range(len(tempPic2))):
+    for nx,tx in zip(range(len(tempPic1[ny])),range(len(tempPic2[ty]))):
+      for ncol,tcol in zip(range(len(tempPic1[ny][nx])),range(len(tempPic2[ty][nx]))):
+        temp = abs(int(float(tempPic1[ty][tx][tcol])) + int(float(tempPic2[ny][nx][ncol])))
+        if(temp <= 255 and temp >= threshold):
+          tempPic1[ty][tx][tcol] = temp
+        elif(temp > 255):
+          tempPic1[ty][tx][tcol] = 255
+        else:
+          tempPic1[ty][tx][tcol] = int(float(tempPic1[ty][tx][tcol]))
+  return tempPic1

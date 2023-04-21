419893abe30dfb13e6ba4518aff9cb8ecf1562fa
dendersen <dendersen@endersen.dk>
2023-04-21 11:55:18 +0200

    prety

--- testSharp.py
+++ testSharp.py
@@ -1,7 +1,7 @@
+import math
 from SRC.image.imageEditor import sharpen, printProgressBar
 from SRC.image.imageLoader import loadImgAsArr
 from matplotlib import pyplot as plt
-from cv2 import cvtColor, COLOR_BGR2RGB
 from time import time
 
 images = []
@@ -11,11 +11,11 @@ for i,img in enumerate(allItems):
   pic = img[0]
   pic = sharpen(pic,threshold=5,showSteps=False,strength=1.2,amplification = 1.03)#keep amplification as low as possible!! or the original image will shine through, can be mitigated with threshold
   printProgressBar(i,len(allItems),tim)
-
+  images.append(pic)
+print("\r" + (" "*100) + "\rdone")
 fig = plt.figure(dpi=300)
 
-size = int(len(images)/2)
-
+size = int(math.ceil(math.sqrt(len(images))))
 for i, image in enumerate(images):
   fig.add_subplot(size,size,i+1)
   plt.imshow(image, cmap="gray")

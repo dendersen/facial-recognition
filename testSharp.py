f36b063c2a625bb76bd4e820093bf8ab424b3502
dendersen <dendersen@endersen.dk>
2023-04-21 11:59:41 +0200

    testing

--- testSharp.py
+++ testSharp.py
@@ -6,7 +6,7 @@ from time import time
 
 images = []
 tim  = time()
-allItems = loadImgAsArr(10,True,cropOri=True)
+allItems = loadImgAsArr(10,False,cropOri=True)
 for i,img in enumerate(allItems):
   pic = img[0]
   pic = sharpen(pic,threshold=5,showSteps=False,strength=1.2,amplification = 1.03)#keep amplification as low as possible!! or the original image will shine through, can be mitigated with threshold
@@ -16,9 +16,12 @@ print("\r" + (" "*100) + "\rdone")
 fig = plt.figure(dpi=300)
 
 size = int(math.ceil(math.sqrt(len(images))))
-for i, image in enumerate(images):
-  fig.add_subplot(size,size,i+1)
-  plt.imshow(image, cmap="gray")
+for i, image in enumerate(zip(images,allItems)):
+  fig.add_subplot(size,size*2,(i+1)*2)
+  plt.imshow(image[0], cmap="gray")  
+  plt.axis("off")
+  fig.add_subplot(size,size*2,(i+1)*2+1)
+  plt.imshow(image[1], cmap="gray")
   plt.axis("off")
 plt.show()
 

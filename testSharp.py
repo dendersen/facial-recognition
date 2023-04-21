27a04e2abe5c9622e17441db848255093e98acd3
dendersen <dendersen@endersen.dk>
2023-04-21 11:47:31 +0200

    more testing

--- testSharp.py
+++ testSharp.py
@@ -1,28 +1,23 @@
-from SRC.image.imageEditor import sharpen
+from SRC.image.imageEditor import sharpen, printProgressBar
 from SRC.image.imageLoader import loadImgAsArr
 from matplotlib import pyplot as plt
 from cv2 import cvtColor, COLOR_BGR2RGB
+from time import time
 
 images = []
-
-for i,img in enumerate(loadImgAsArr(100,True,cropOri=True)):
-  
+tim  = time()
+allItems = loadImgAsArr(10,True,cropOri=True)
+for i,img in enumerate(allItems):
   pic = img[0]
-  
-  # imshow('Cam output: ', pic)
-  print(i,end="\r")
-  
   pic = sharpen(pic,threshold=5,showSteps=False,strength=1.2,amplification = 1.03)#keep amplification as low as possible!! or the original image will shine through, can be mitigated with threshold
-  images.append(cvtColor(pic,COLOR_BGR2RGB))
-  # imshow(f'sharp output: ',pic)
-  
+  printProgressBar(i,len(allItems),tim)
 
 fig = plt.figure(dpi=300)
 
 size = int(len(images)/2)
 
 for i, image in enumerate(images):
-  fig.add_subplot(size,size,i)
+  fig.add_subplot(size,size,i+1)
   plt.imshow(image, cmap="gray")
   plt.axis("off")
 plt.show()

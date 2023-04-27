0f02ab54b5dcae3b294667512473097415ca717a
Chrisser1 <chrisgthomsen0310@gmail.com>
2023-04-27 15:53:07 +0200

    works!
    Co-authored-by: dendersen <dendersen@users.noreply.github.com>

--- trainSiameseNetwork.py
+++ trainSiameseNetwork.py
@@ -1,9 +1,12 @@
-# import SRC.AI.simpleNeuralNetwerk.simpleAi as simpleNeuralNetwork
 from typing import List
 from matplotlib import pyplot as plt
 from SRC.AI.siameseAI import SiameseNeuralNetwork
 import pandas as pd
 from typing import Union
+import itertools
+from SRC.progBar import progBar
+import os
+import platform
 
 def saveData(losses: Union[List[float],None], test_accuracy: Union[List[float],None] = None, train_accuracy: Union[List[float],None] = None, label = "Christoffer"):
   if losses == None:
@@ -31,22 +34,64 @@ def modelAcc(label: str):
   axes[1].legend()
   plt.show()
 
-label:str = "Niels"
+def clear_screen():
+    if platform.system().lower() == "windows":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-Network = SiameseNeuralNetwork(
-  person = label,
-  loadOurData  = True,
-  loadAmount = 2000,
-  varients = 5,
-  learning_rate = 1e-3,
-  trainDataSize = .9,
-  batchSize = 64,
-  reprocessDataset = False,
-  useDataset = True,
-  resetNetwork = False,
-  networkSummary = False
-)
+def trainAndEvaluate(learningRate, lamdaReg, label, EPOCHS=50, loadNewData = False, resetNetwork = True):
+  network = SiameseNeuralNetwork(
+    person=label,
+    loadNewData=loadNewData,
+    loadAmount=2000,
+    varients=5,
+    learningRate=learningRate,
+    lamdaReg=lamdaReg,
+    trainDataSize=0.9,
+    batchSize=64,
+    reprocessDataset=False,
+    useDataset=True,
+    resetNetwork=resetNetwork,
+    networkSummary=False
+  )
+  
+  trainLossResults,testAccuracyResults,trainAccuracyResults = network.train(EPOCHS)
+  saveData(trainLossResults,testAccuracyResults,trainAccuracyResults, label=label)
+  return testAccuracyResults[-1]
 
-saveData(*Network.train(50),label=label)
+def findBesthyperparameters():
+  # Definer intervallerne for hyperparametre
+  learning_rate_values = [1e-2, 1e-3, 1e-4, 1e-5]
+  lambdaReg_values = [1e-3, 1e-4, 1e-5, 1e-6]
+
+  # Kombiner alle mulige værdier af hyperparametre
+  hyperparameter_combinations = list(itertools.product(learning_rate_values, lambdaReg_values))
+
+  # Variabler til at holde den bedste kombination og resultater
+  best_hyperparameters = None
+  best_val_accuracy = 0
+
+  label = "Niels"
+  EPOCHS = 10
+  progbar = progBar(len(learning_rate_values)*len(lambdaReg_values),prefix="\033[4Aall calculations:")
+  for i, var in enumerate(hyperparameter_combinations):
+    if(i == 1):
+      # Call the function to clear the terminal
+      clear_screen()
+      progbar.print(i,suffix="\n\n")
+    lr, lambdaReg = var
+    # progbar.print(i,suffix="\n")
+    print(f"Training with learning rate: {lr}, lambdaReg: {lambdaReg}")
+    # Træn og evaluer modellen med de aktuelle hyperparametre
+    val_accuracy = trainAndEvaluate(lr, lambdaReg, label, EPOCHS)
+
+    # Hvis nøjagtigheden på valideringsdata er bedre end den hidtil bedste, opdater den bedste kombination og resultater
+    if val_accuracy > best_val_accuracy:
+        best_val_accuracy = val_accuracy
+        best_hyperparameters = (lr, lambdaReg)
+    progbar.print(i+1,suffix="\n\n")
+  print(f"Best hyperparameters: learning_rate: {best_hyperparameters[0]}, lambdaReg: {best_hyperparameters[1]}")
+label = "Christoffer"
+trainAndEvaluate(learningRate=0.0001,lamdaReg=1e-6, label=label, EPOCHS=10, loadNewData=True, resetNetwork=False)
 modelAcc(label)
-Network.makeAPredictionOnABatch()
